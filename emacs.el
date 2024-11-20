59c21f3618f41fcf8c4d811f19feea26c78fb41c
aiffc <yjc18915568435@gmail.com>
2024-11-20 20:07:11 +0800

    rename

--- emacs.el
+++ emacs.el
@@ -138,3 +138,15 @@
   (c-mode . lsp-deferred)
   (c++-mode . lsp-deferred)
   (c++-mode . c-toggle-hungry-state))
+
+(use-package projectile
+  :ensure t
+  :bind (("C-c p" . projectile-command-map))
+  :config
+  (setq projectile-mode-line "Projectile")
+  (setq projectile-track-known-projects-automatically nil))
+
+(use-package counsel-projectile
+  :ensure t
+  :after (projectile)
+  :init (counsel-projectile-mode))
